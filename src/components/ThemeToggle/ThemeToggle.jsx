5271d6f9a2f3eba79af6b5c6616790ac0e6c2956
Ashil Thomas <142432081+ashilthomas@users.noreply.github.com>
2025-08-13 13:45:46 +0530

    update colour darkmode button

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { useTheme } from '../../context/ThemeContext';
+import React from "react";
+import { useTheme } from "../../context/ThemeContext";
+import { Moon, Sun } from "lucide-react";
 
 function ThemeToggle() {
-      const { theme, toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
   return (
+    <>
       <button
-      onClick={toggleTheme}
-      className="px-3 py-1 bg-gray-300 dark:bg-gray-700 rounded cursor-pointer"
-    >
-      {theme === "dark" ? "🌙 Dark" : "☀️ Light"}
-    </button>
-  )
+        onClick={toggleTheme}
+        className=" items-center rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer"
+      >
+        {theme === "dark" ? (
+         <Moon  className="w-6 h-6 text-[#7D7BFF] fill-[#7D7BFF]"/>
+        ) : (
+         <Sun className="w-6 h-6 file:bg-yellow-500 text-yellow-500" />
+        )}
+      </button>
+    </>
+  );
 }
 
-export default ThemeToggle
+export default ThemeToggle;
